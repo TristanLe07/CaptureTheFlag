ba1b582f8383ea96fd6857e22423b5c314958825
TristanLe07 <107384818+TristanLe07@users.noreply.github.com>
2024-08-20 15:05:54 +0800

    THIS GUY STINKS

--- Objects/Red2.py
+++ Objects/Red2.py
@@ -1,13 +1,17 @@
 from GameFrame import RedBot, Globals
 from enum import Enum
+import random
 
 class STATE(Enum):
     SEARCH_JAILED_BOT = 1
     FREE_JAILED_BOT = 2
-
+    GUARD = 3
+    
 class Red2(RedBot):
     def __init__(self, room, x, y):
         RedBot.__init__(self, room, x, y)
+        self.initial_wait = random.randint(30, 90)
+        self.wait_count = 0
         self.curr_state = STATE.SEARCH_JAILED_BOT
 
     def tick(self):
@@ -16,10 +20,16 @@ class Red2(RedBot):
                 if bot.jailed:
                     self.curr_state = STATE.FREE_JAILED_BOT
                     self.turn_towards(bot.x, bot.y, Globals.FAST)
+                    print(self.curr_state)
                     break
+                else:
+                    self.turn_towards(Globals.blue_flag.x, Globals.blue_flag.y, Globals.FAST)
+                    self.drive_forward(Globals.FAST)
+                    pass
         elif self.curr_state == STATE.FREE_JAILED_BOT:
             self.drive_forward(Globals.FAST)
             for bot in Globals.red_bots:
                 if self.point_to_point_distance(self.x, self.y, bot.x, bot.y) < 10:
                     self.curr_state = STATE.SEARCH_JAILED_BOT
-                    break
+                    print(self.curr_state)
+                    pass
